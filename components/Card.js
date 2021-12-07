7a19631ee131b9ab6d2081fc58836489427f58b9
Sumesh <sumesh.majhi.5@gmail.com>
2021-12-07 09:22:59 +0530

    📦 NEW: Add Card UI

---
+++ components/Card.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const Card = styled.div`
+  background-color: #fff;
+  border-radius: 6px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.2);
+`;
+
+export default Card;
